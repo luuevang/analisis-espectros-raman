b27ebe4ffccff69731c5818dd86102a55266f705
Lucía <luciaevangelistagallo@gmail.com>
2021-06-09 13:28:24 -0300

    .

--- figura.py
+++ figura.py
@@ -8,8 +8,8 @@ ydata = datos[:,1]
 
 fig = plt.figure(figsize = (4, 2))
 plt.plot(xdata, ydata)
-plt.legend(['Datos'], fontsize = 18)
-plt.xlabel('Raman Shift [cm$^{-1}$]', fontsize = 18, ha = 'center')
-plt.ylabel('Intensidad [u.a.]', fontsize = 18, ha = 'center')
-plt.title('Espectro sin analizar', fontsize = 18, ha = 'center')
+plt.legend(['Datos'], fontsize = 24)
+plt.xlabel('Raman Shift [cm$^{-1}$]', fontsize = 24, ha = 'center')
+plt.ylabel('Intensidad [u.a.]', fontsize = 24, ha = 'center')
+plt.title('Espectro sin analizar', fontsize = 24, ha = 'center')
 plt.show()
